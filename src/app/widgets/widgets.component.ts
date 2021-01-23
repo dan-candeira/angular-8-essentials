74bcce65b6bc8cfb9ce9b2cda0aac3c9a95e8f48
dan-candeira <danielbarr16@gmail.com>
2021-01-23 19:28:32 -0300

    refactor(widget): add conection to json API

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,30 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-import { Widget } from '../shared/widget.model';
-import { WidgetsService } from '../shared/widgets.service';
+import { Widget, WidgetsService } from '../shared';
 
 @Component({
   selector: 'app-widgets',
   templateUrl: './widgets.component.html',
-  styleUrls: ['./widgets.component.scss']
+  styleUrls: ['./widgets.component.scss'],
 })
 export class WidgetsComponent implements OnInit {
-  selectedWidget;
-
+  selectedWidget: Widget;
+  readonly = false;
   widgets: Widget[];
 
-  constructor(private widgetsService: WidgetsService) { }
+  constructor(private widgetsService: WidgetsService) {}
 
   ngOnInit() {
     this.getWidgets();
+    this.resetCurrentWidget();
   }
 
   getWidgets() {
-    this.widgetsService.all()
-      .subscribe(widgets => this.widgets = widgets);
+    this.widgetsService.all().subscribe((widgets) => (this.widgets = widgets));
   }
 
   selectWidget(widget) {
     this.selectedWidget = widget;
   }
 
+  resetCurrentWidget() {
+    this.selectedWidget = { id: null, name: '', description: '' };
+  }
+
+  cancel() {
+    this.resetCurrentWidget();
+  }
+
+  save(widget: Widget) {
+    if (!widget.id) {
+      this.createWidget(widget);
+    } else {
+      this.updateWidget(widget);
+    }
+  }
+
+  createWidget(widget: Widget) {
+    if (widget.name.length !== 0) {
+      this.widgetsService.create(widget).subscribe((response) => {
+        this.getWidgets();
+        this.resetCurrentWidget();
+      });
+    }
+  }
+
+  updateWidget(widget: Widget) {
+    this.widgetsService.update(widget).subscribe((respoonse) => {
+      this.getWidgets();
+      this.resetCurrentWidget();
+    });
+  }
+
+  deleteWidget(widget) {
+    this.widgetsService.delete(widget).subscribe((response) => {
+      this.getWidgets();
+      this.resetCurrentWidget();
+    });
+  }
 }
